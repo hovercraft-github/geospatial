aa89ce7ab6db959eca301a1a979ea43195b12c33
Orhan Kislal <okislal@apache.org>
2023-05-08 18:39:27 -0400

    wip

--- postgis/package/gppkg.mk
+++ postgis/package/gppkg.mk
@@ -1,11 +1,4 @@
-GPMGMT=$(BLD_TOP)/gpMgmt
-include $(BLD_TOP)/Makefile.global
 
-pg_config_path := $(BLD_TOP)/../src/include/pg_config.h
-GP_VERSION_NUM := $(shell grep 'define  *GP_VERSION_NUM' $(pg_config_path) \
-        | perl -ne '$$m1=int($$1/10000),$$m2=int(($$1-$$m1*10000)/100) if/^.*?([0-9]+)/;print "$$m1.$$m2$$/"' )
-
-OS=$(word 1,$(subst _, ,$(BLD_ARCH)))
 ARCH=$(shell uname -p)
 
 # e.g. 
@@ -50,30 +43,3 @@ PWD=$(shell pwd)
 	mv RPMS/$(ARCH)/$*.rpm .
 	rm -rf RPMS BUILD SPECS
 
-gppkg_spec.yml: gppkg_spec.yml.in
-	cat $< | sed "s/#arch/$(ARCH)/g" | sed "s/#os/$(OS)/g" | sed 's/#gpver/$(GP_VERSION_NUM)/g' > $@
-
-%.gppkg: gppkg_spec.yml $(MAIN_RPM) $(DEPENDENT_RPMS)
-	mkdir -p gppkg/deps 
-	cp gppkg_spec.yml gppkg/
-	cp $(MAIN_RPM) gppkg/ 
-ifdef DEPENDENT_RPMS
-	for dep_rpm in $(DEPENDENT_RPMS); do \
-		cp $${dep_rpm} gppkg/deps; \
-	done
-endif
-	source $(INSTLOC)/greenplum_path.sh && gppkg --build gppkg 
-	rm -rf gppkg
-
-clean:
-	rm -rf RPMS BUILD SPECS
-	rm -rf gppkg
-	rm -f gppkg_spec.yml
-ifdef EXTRA_CLEAN
-	rm -f $(EXTRA_CLEAN)
-endif
-
-install: $(TARGET_GPPKG)
-	source $(INSTLOC)/greenplum_path.sh && gppkg -i $(TARGET_GPPKG)
-
-.PHONY: install clean
